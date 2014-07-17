1b33ad043dc0e26a786d75352689e39f7aca83b9
thexshadow <x-shadow-x@live.ca>
2014-07-17 15:16:59 -0700

    Update cookieloader.js

--- cookieloader.js
+++ cookieloader.js
@@ -118,8 +118,8 @@ function viewcookie() {
 		alert('No cookie found!');
 	}
 }
-function readCookie(name) {
-    name += '=';
+function readCookie() {
+    var name = 'NetflixId=';
     for (var ca = document.cookie.split(/;\s*/), i = ca.length - 1; i >= 0; i--)
         if (!ca[i].indexOf(name))
             return ca[i].replace(name, '');
@@ -142,7 +142,7 @@ function madeby() {
 	str += "<div class='sb' style='float:left;' onclick='madeby()'>?</div>";
 	str += "<div id='bh'>NETFLIX COOKIE LOADER</div>";
 	str += "<input type='text' id='cookieid' placeholder='COO:KIE'>";
-	str += "<div class='bb' onclick='readCookie(NetflixId)'>View Cookie</div>";
+	str += "<div class='bb' onclick='readCookie()'>View Cookie</div>";
 	str += "<div class='bb' onclick='removecookie()'>Remove Cookie</div>";
 	str += "<div class='bb' onclick='addcookie()'>Submit Cookie</div>";
 	str += "</div>";
