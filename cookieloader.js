6a7d003545227e38d1f85b827990ecf522b3fd11
thexshadow <x-shadow-x@live.ca>
2014-07-17 03:18:12 -0700

    Update cookieloader.js

--- cookieloader.js
+++ cookieloader.js
@@ -117,9 +117,8 @@ function viewcookie() {
 		for(var i=0;i < ca.length;i++) {
 			var c = ca[i];
 			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(id1) == 0)
-			if (c.indexOf(id2) == 0)
-			alert('SecureNetflixId:\n' + c.substring(id2.length,c.length) + 'NetflixId:\n' + c.substring(id1.length,c.length));
+			if (c.indexOf(id1) == 0) alert('NetflixId:\n' + c.substring(id1.length,c.length));
+			if (c.indexOf(id2) == 0) alert('SecureNetflixId:\n' + c.substring(id2.length,c.length));
 		}
 	}
 	else {
