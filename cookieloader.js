5b8937dac33845dabf206e76a4aabf9dfd82127d
thexshadow <x-shadow-x@live.ca>
2014-07-17 19:11:17 -0700

    Update cookieloader.js

--- cookieloader.js
+++ cookieloader.js
@@ -139,7 +139,7 @@ function madeby() {
 	str += "<div class='sb' style='float:left;' onclick='madeby()'>?</div>";
 	str += "<div id='bh'>NETFLIX COOKIE LOADER</div>";
 	str += "<input type='text' id='cookieid' placeholder='COO:KIE'>";
-	str += "<div class='bb' onclick='viewcookie1()'>View Cookie</div>";
+	str += "<div class='bb' onclick='viewcookie()'>View Cookie</div>";
 	str += "<div class='bb' onclick='removecookie()'>Remove Cookie</div>";
 	str += "<div class='bb' onclick='addcookie()'>Submit Cookie</div>";
 	str += "</div>";
