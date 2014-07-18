96537fa904fe3b3a9db07ed4800aaec1903a8689
thexshadow <x-shadow-x@live.ca>
2014-07-17 18:32:29 -0700

    Update cookieloader.js

--- cookieloader.js
+++ cookieloader.js
@@ -121,14 +121,7 @@ function viewcookie() {
 
 
 function viewcookie1() {
-    var name = "netflixid=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) != -1) alert(c.substring(name.length,c.length));
-    }
-    return "";
+    alert('tetet')
 } 
 
 
